6babffd05daab89ea4b25385fd8fc862cc944c49
khasenovweb <hasenovkirill93@gmail.com>
2020-08-27 19:43:20 +0500

    dfkjdhfkjh

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: [
+      '<img src="img/slider-arrow-left.svg">',
+      '<img src="img/slider-arrow-right.svg" >',
+    ],
+  });
+});
